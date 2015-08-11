78266d9c4a8a435fd32d5e9d5fd25ef10d368d6c
Stefan Walther <swr@qlik.com>
2015-08-11 23:52:40 +0200

    Move over to sense-go

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+'use strict';
+var gulp = require('gulp');
+var senseGo = require('sense-go');
+
+var userConfig = {
+	"packageName": "sense-themable-kpi-tile"
+};
+
+senseGo.init( gulp, userConfig,  function (  ) {
+
+});
